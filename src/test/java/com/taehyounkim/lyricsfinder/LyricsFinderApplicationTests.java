62147f0815885b1487999fdc6f11bc80549e65de
TaeHyoun Kim <kth9903@gmail.com>
2024-04-30 14:13:36 +0900

    lyricsfinder 프로젝트 생성

---
+++ src/test/java/com/taehyounkim/lyricsfinder/LyricsFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taehyounkim.lyricsfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LyricsFinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
